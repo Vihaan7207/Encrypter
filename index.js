3d580844ade0e902fe40d1586e980dc12fc70be8
Vihaan Mikkilineni <vmikki7207@gmail.com>
2022-10-23 12:02:13 -0400

    added function to ask for the message

--- index.js
+++ index.js
@@ -15,7 +15,21 @@ const welcome = async () => {
 
     await sleep(2.5);
     title.stop();
+    console.log(`
+        ${chalk.green('JS Encrypter')}
+        ${chalk.blueBright('Put in a message and the program will encrypt it for you')}
+    `)
+}
 
+const askMsg = async () => {
+    const message = await inquirer.prompt({
+        name: "msg-answer",
+        type: "input",
+        message: "What is the message you would like to encrypt",
+        default(){
+            return "Hello world!"
+        }
+    })
 }
 
 await welcome();
