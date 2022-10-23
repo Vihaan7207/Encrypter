a3a6ed3c68e2ea97c55197a27b30971bdaab7e9c
Vihaan Mikkilineni <vmikki7207@gmail.com>
2022-10-23 11:44:10 -0400

    some more

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 import chalkAnimation from 'chalk-animation'
 
+let msg;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
